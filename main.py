27d2f72afe95a2f163200dc923fed82d8923a9c7
HarisGGEZ <haris.sauban@gmail.com>
2024-04-23 11:00:09 +0200

    create text changer

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import tkinter as tk
 from tkinter import *
+import map
+Map = map.kartaKlass()
+
+def moving():
+    choice = Map.movement()
+    Map.move(choice)
+    Map.returnLocation()
 
 def textChanger():
-    chosenText = "text"
-    textBox.config(text=chosenText)
+    moving()
+    chosenText = Map.printChoice()
+    textBox.config(text=chosenText, justify= LEFT)
     root.after(300, textChanger)
 
 
@@ -15,11 +23,11 @@ img = PhotoImage(file="layout.png")
 tk.Label(image=img).place(relx=0.33, rely=0.05)
 
 textBox = tk.Label(text="helloo", font=("TkDefaultFont", 25))
-textBox.place(relx=0.1, rely=0.05)
-
+textBox.place(relx=0.05, rely=0.05)
 
 
 
 
 root.after(300, textChanger)
+
 root.mainloop()
